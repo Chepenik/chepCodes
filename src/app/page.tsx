e9e4811d231d94ddaf3886a008d0aaef42deb596
Conor <chepenikconor@gmail.com>
2023-09-19 14:41:21 -0400

    updated card to look better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="bg-gray-100">
       <Header />
-      <main className="max-w-4xl mx-auto p-4 flex flex-col items-center">
+      <main className="mx-auto p-4 flex flex-col items-center">
       <HomeContent />
       <br />
       <br />
